7a74b142457a67a59e74ecae28d7a79627a7adf0
ndelanhese <nnathanh3@gmail.com>
2023-11-14 14:04:46 -0300

    fix(docs)add storybook static on ignore

--- packages/docs/storybook-static/assets/MultiStep.stories.b87e5533.js
+++
@@ -1,2 +0,0 @@
-import{M as t,B as e}from"./index.bbe86e93.js";import{j as o}from"./jsx-runtime.c7885a3d.js";import"./index.7fdbbddf.js";import"./iframe.f7238817.js";import"./index.9ca3484e.js";const m={title:"Form/Multi Step",component:t,args:{size:4,currentStep:1},decorators:[r=>o(e,{as:"label",css:{display:"flex",flexDirection:"column",gap:"$2"},children:r()})]},n={args:{}},c={args:{currentStep:4}},u=["Primary","Full"];export{c as Full,n as Primary,u as __namedExportsOrder,m as default};
-//# sourceMappingURL=MultiStep.stories.b87e5533.js.map
